e0f14cbf1e79efdfeaa87c9c7d3a646a42460f11
Guinslym <guinslym@gmail.com>
2017-06-17 14:53:21 -0400

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('emploi.base')
+  @section('content')
+  <div class="row">
+  	<div class="col-md-12">
+		<h1>404 page</h1>
+  
+  <br>
+    <p>We couldn't find the job or the page requested.</p>
+    <p>click  <a href="/">here</a> to return to the homepage</p>
+
+  	</div>
+  </div>
+
+  @stop
+
+
+
+
