1b4ccf2548ff55450b804bd0a5e19eeaa660b3f7
Guinslym <guinslym@gmail.com>
2017-06-17 14:43:21 -0400

    adding vendor

---
+++ resources/views/vendor/humans/humans.blade.php
@@ -0,0 +1,11 @@
+/* TEAM */
+Developer: Guinsly Mondesir
+Twitter: @guinslym
+From: Ottawa, Ontario, Canada
+
+/* SITE */
+Last update: {{ date('Y-m-d') }}
+Language: English, French
+Doctype: HTML5
+Framework: Laravel
+IDE: Sublime Text 3
